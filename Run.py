035530f42a1089ac1fcdd3e33ba186122b2e3e0a
Yanwei Xu <102143672+dawei7777@users.noreply.github.com>
2025-03-14 11:52:13 +0800

    Add files via upload

--- Run.py
+++ Run.py
@@ -1,23 +1,13 @@
 
 import time
-import matplotlib.colors as mcolors
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
-import torch.optim as optim
-import torch.utils.data as data_utils
-from torch.autograd import Variable
-import math
 import torchvision.models as models
-from PIL import Image
-import os 
 from torchvision import datasets 
 from torch.utils.data import DataLoader  
 from YOAO import YOAO
+from DeepFool import deepfool
 # 设定设备  
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
   
@@ -34,18 +24,30 @@ def clip_tensor(A, minv, maxv):
     A = torch.min(A, maxv*torch.ones(A.shape))
     return A
 
+def run_attack(attack_type,image, net, num_classes, overshoot, max_iter):
+    if attack_type == 1:
+        r, loop_i, label_orig, label_pert, pert_image = YOAO(image, net, num_classes, overshoot, max_iter)
+    elif attack_type == 2:
+        r, loop_i, label_orig, label_pert, pert_image = deepfool(image, net, num_classes, overshoot, max_iter)
+    else:
+        raise ValueError("Invalid attack type. Please choose 1 for YOAO or 2 for DeepFool.")
+    return r, loop_i, label_orig, label_pert, pert_image
+
+max_iter = 1
+overshoot = 1
+num_classes=10
 mean = [ 0.485, 0.456, 0.406 ]
 std = [ 0.229, 0.224, 0.225 ]
 test_data = datasets.CIFAR10(root='./data/', train=False,download=True, 
                              transform= transforms.Compose([  
                                                transforms.ToTensor(),  
-                                               transforms.Normalize(mean=mean, std=std)  # 归一化  
+                                               transforms.Normalize(mean=mean, std=std)  # 归一化
                                                ])  )
 
 
 class_labels_en = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-
+attack_type = int(input("请选择攻击类型，输入数字1、2（1: YOAO, 2: DeepFool）: "))
 for m in range(1):
     test_loader = DataLoader(test_data, batch_size=1, shuffle=True)  # 创建一个数据加载器 
     count=0
@@ -57,8 +59,8 @@ for m in range(1):
 
         count=count+1
         images, labels = images.to(device), labels.to(device)  
-        for i, (image, label) in enumerate(zip(images, labels)):    
-            r, loop_i, label_orig, label_pert, pert_image = YOAO(image, model_ResNet18,10,0.02,1,0)
+        for i, (image, label) in enumerate(zip(images, labels)):
+            r, loop_i, label_orig, label_pert, pert_image = run_attack(attack_type,image, model_ResNet18, num_classes, overshoot,max_iter)
             print("Original label = ", label_orig)
             print("Perturbed label = ", label_pert) 
             if label_orig!=label_pert:
